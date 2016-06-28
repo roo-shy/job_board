3708e731d8ad1ee89a4726745a76cac7f16a8b95
Rooshy <ru_sha_nia@yahoo.ca>
2016-06-27 23:22:25 -0500

    Editing Jobs Controller, new and create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,38 @@
 class JobsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jobs_params)
+
+    if @job.save
+      redirect_to @job
+    else
+      render "New"
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def jobs_params
+  end
+
+  def find_job
+  end
+
 end
