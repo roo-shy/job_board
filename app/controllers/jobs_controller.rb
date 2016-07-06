659d3c38430eca32c18314b986513aa585d032f3
Rooshy <ru_sha_nia@yahoo.ca>
2016-07-05 22:40:16 -0500

    Permiting job category to be inputed in the form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -43,7 +43,7 @@ class JobsController < ApplicationController
   private
 
   def jobs_params
-    params.require(:job).permit(:title, :description, :company, :url)
+    params.require(:job).permit(:title, :description, :company, :url, :category_id)
   end
 
   def find_job
