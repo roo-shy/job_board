0831c44ad3b3a2777ce0ea4fb552b666a5dc2e66
Rooshy <ru_sha_nia@yahoo.ca>
2016-07-05 23:16:45 -0500

    Adding HAML style navigation

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,12 @@ class JobsController < ApplicationController
 
 
   def index
+   if params[:category].blank?
     @jobs = Job.all.order("created_at DESC")
+   else
+    @category_id = Category.find_by(name: params[:category]).id
+    @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+   end
   end
 
   def show
